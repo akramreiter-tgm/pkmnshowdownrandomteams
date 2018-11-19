653184260296e8a10294f85ea7f7a71bdaa7b6ce
KlausRuediger :^) <akramreiter@student.tgm.ac.at>
2018-11-19 08:16:43 +0100

    Hotfixes + switch to Momo's script

--- src/Teamgenerator.py
+++ src/Teamgenerator.py
@@ -1,62 +1,108 @@
 import json
+import argparse
 from random import randint
+from pathlib import Path
+import os
 
-def printoutput(mon):
-    print(mon["name"] + " @ " + mon["item"])
-    print("Ability: " + mon["ability"])
-    if mon["level"] > 0:
-        print("Level: " + str(mon["level"]))
-    if len(mon["ev"]) > 0:
-        print ("EVs: " + mon["ev"])
-    print(mon["nature"] + " Nature")
-    if len(mon["iv"]) > 0:
-        print ("IVs: " + mon["iv"])
-    for x in mon["moves"]:
-        print("- " + x)
-    pass
+parser = argparse.ArgumentParser()
+
+# Extend if needed
+parser.add_argument("-w", "--write",
+                    help="If 'True' it generates an output file in the local 'C:/Users/<USER>/Documents/My Games/Pokemon Showdown/Teams/full randomized' folder",
+                    type=bool, required=False, default=False, nargs='?', const=True)
+
+args = parser.parse_args()
+
+
+def returnoutput(mons):
+    """
+    Returns the generated Pokémon in the right format.
+    """
+    outtext = ""
+    for mon in mons:
+        outtext += mon["name"] + " @ " + mon["item"] + "\n"
+        outtext += "Ability: " + mon["ability"] + "\n"
+        if mon["level"] > 0:
+            outtext += "Level: " + str(mon["level"]) + "\n"
+        if len(mon["ev"]) > 0:
+            outtext += "EVs: " + mon["ev"] + "\n"
+        outtext += mon["nature"] + " Nature" + "\n"
+        if len(mon["iv"]) > 0:
+            outtext += "IVs: " + mon["iv"] + "\n"
+        for x in mon["moves"]:
+            outtext += "- " + x + "\n"
+        outtext += "\n"
+    return (outtext)
+
+
+def writeoutput(mons, out):
+    """
+    Writes the generated Pokémon to a file in the given directory. DOES ONLY WORK WITH THE DESKTOP APP ATM
+    TODO: find a solution for browser version
+    """
+    outpath = "%s\\Documents\\My Games\\Pokemon Showdown\\Teams\\%s" % (Path.home(), out)
+    numberset = []
+    outfilename = "[gen7] Generated %s " % out
+
+    for filename in os.listdir(outpath):
+        if filename.endswith(".txt"):
+            filename = filename[:-4]
+            filename = filename[len(outfilename):]
+            numberset.append(int(filename))
+
+    i = 1
+    for n in numberset:
+        if i + 1 == n:
+            break
+        i += 1
+
+    with open("%s/[gen7] Generated %s %i.txt" % (outpath, out, i), "w") as outfile:
+        outfile.write(returnoutput(mons))
+
 
 def fullrandom():
+    """
+    TODO: Doc comments
+    """
     with open("fullrandom.json") as source:
         temp = json.load(source)
-        pkmn = temp["pkmn"]
-        moves = temp["moves"]
-        ability = temp["ability"]
-        item = temp["item"]
-        nature = temp["nature"]
-        mons = [];
-        mons.append({});
-        mons.append({});
-        mons.append({});
-        mons.append({});
-        mons.append({});
-        mons.append({});
-        for x in mons:
-            x["ev"] = "252 HP / 252 Def / 252 SpD / 252 Spe"
-            x["name"] = pkmn[randint(0,len(pkmn)-1)]
-            pkmn.remove(x["name"])
-            x["nature"] = nature[randint(0,len(nature)-1)]
-            nature.remove(x["nature"])
-            x["ability"] = ability[randint(0,len(ability)-1)]
-            ability.remove(x["ability"])
-            x["level"] = 0
-            x["iv"] = ""
-            x["item"] = item[randint(0, len(item) - 1)]
-            item.remove(x["item"])
-            x["moves"] = []
-            for y in range(0,4):
-                x["moves"].append(moves[randint(0, len(moves) - 1)])
-                moves.remove(x["moves"][y])
+    pkmn = temp["pkmn"]
+    moves = temp["moves"]
+    ability = temp["ability"]
+    item = temp["item"]
+    nature = temp["nature"]
+    mons = []
+    for x in range(0, 6):
+        mons.append({})
+    for x in mons:
+        x["ev"] = "252 HP / 252 Def / 252 SpD / 252 Spe"
+        x["name"] = pkmn[randint(0, len(pkmn) - 1)]
+        pkmn.remove(x["name"])
+        x["nature"] = nature[randint(0, len(nature) - 1)]
+        nature.remove(x["nature"])
+        x["ability"] = ability[randint(0, len(ability) - 1)]
+        ability.remove(x["ability"])
+        x["level"] = 0
+        x["iv"] = ""
+        x["item"] = item[randint(0, len(item) - 1)]
+        item.remove(x["item"])
+        x["moves"] = []
+        for y in range(0, 4):
+            x["moves"].append(moves[randint(0, len(moves) - 1)])
+            moves.remove(x["moves"][y])
 
-        for x in mons:
-            printoutput(x)
-            print("")
+    print(returnoutput(mons))
+    print("")
+
+    if (args.write):
+        writeoutput(mons, "full randomized")
 
 
-
-def generateteams():
-    pkmn = []
-    fullrandom()
-    exit(0)
+def alexrandom():
+    """
+    TODO: Tell me Alex ~~~
+    """
+    """
     with open("generate.json") as source:
         pkmn = json.load(source)["pkmn"]
 
@@ -76,7 +122,16 @@ def generateteams():
     for x in pkmn:
         print(x)
     printoutput(temp)
+    """
     pass
+
+
+def generateteams(case=0):
+    """
+    Calls another function. Only exists for possible UI tbh.
+    """
+    if case == 0:
+        fullrandom()
 
 
 if __name__ == '__main__':
